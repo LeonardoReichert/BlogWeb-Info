38a746f7f97a4d4958ee4d5c96b9b636c0a97f93
Leonardo Reichert <leoreichert5000@gmail.com>
2022-12-19 05:16:37 -0300

    añadido paginacion a mano compatible con cualquier consulta

--- BlogWeb/BlogWeb/views.py
+++ BlogWeb/BlogWeb/views.py
@@ -5,11 +5,12 @@ from django.urls import reverse
 
 from noticias.models import *;
 
+from math import ceil;
 
 #from django.conf import settings
 #from django.core.files.storage import FileSystemStorage
 
-
+"""
 def inicio(request):
 
     # - juntar info de noticias -
@@ -49,29 +50,99 @@ def inicio(request):
     contexto = {"noticiasVisibles": noticiasVisibles};
 
     return render(request, "inicio.html", contexto);
+"""
 
 
-def registro(request):
+
+
+def _paginarResultados(resultados_consulta, pagina_actual, elementos_visibles):
+    """
+    resultados_consulta:
+        se refiere a todos los ids de una consulta tal como:
+            Tabla.objects.all().order_by("-fecha").values("id")
+                o tambien:
+            Tabla.objects.filter(categoria=id).values("id")
+    
+    pagina_actual:
+        la pagina actual donde esta la vista, puede ser mayor o igual a 1
+    
+    elementos_visibles:
+        cantidad de elementos por vista
+    
+    devuelve:
+    los ids de la pagina
+    pagina anterior, posterior, y maxima
+             la pagina anterior o posterior tambien podria ser 0 para no avanzar
+        la pagina minima siempre sera 1
+    """
+
+    cantidad_resultados = resultados_consulta.count();
+
+    paginaMaxima = ceil(cantidad_resultados / elementos_visibles);
+
+    paginaAnterior = pagina_actual-1 if int(pagina_actual) > 1 else 0;
+    paginaSiguiente = pagina_actual+1 if int(pagina_actual) < paginaMaxima else 0;
+    
+    idsPagina = resultados_consulta[elementos_visibles*paginaAnterior:
+                                    elementos_visibles*pagina_actual];
+
+    return idsPagina, paginaAnterior, paginaSiguiente, paginaMaxima;
+
+
+
+
+def inicio(request):
+
+    # - juntar info de noticias -
+
+#    totalNoticias = Noticia.objects.count();
+#    print("cantidad noticias: ", totalNoticias)
+
+    noticiasVisibles = [];
+
+    SHOW_MAX = 5; #noticias
+    DESC_MAX = 200;
+
+    consulta = Noticia.objects.all().order_by("-fecha").values("id");
+    maxPagina = ceil(consulta.count() / SHOW_MAX);
+
+    #print(consulta.values("titulo"))
+    
+    paginaActual = request.GET.get("pagina", "1");
+    if not paginaActual.isdigit() or int(paginaActual) < 1 or int(paginaActual) > maxPagina:
+        return redirect("inicio");
+    paginaActual = int(paginaActual)
+
+    ids, pAnterior, pSiguiente, pMaxima = _paginarResultados(consulta, paginaActual, SHOW_MAX);
+    
+    
+    for noticia in Noticia.objects.filter(id__in=ids):
+        parte = NoticiaParte.objects.get(noticia=noticia);
+
+        idNoticia = noticia.id;
+        urlImg = parte.urlImagen;
+        titulo = noticia.titulo;
+        fecha = horaUtcToArg(noticia.fecha);
+        if noticia.categoria:
+            categoria = noticia.categoria.nombre;
+        else:
+            #probablemente la categoria fue eliminada
+            categoria = "Sin categoria";
+
+        descripcionCorta = parte.mensaje.strip()[:DESC_MAX].replace("\n\n", "\n");
+
+        noticiasVisibles.append( (idNoticia, titulo, fecha, urlImg,
+                                    categoria, descripcionCorta) );
 
     #parametros a la web template
-    contexto = {};
+    contexto = {"noticiasVisibles": noticiasVisibles,
+                "paginaActual": paginaActual,
+                "paginaAnterior": pAnterior,
+                "paginaSiguiente": pSiguiente,
+                "paginaMaxima": pMaxima,
+                };
 
-    return render(request, "registro.html", contexto);
-
-
-"""
-def login(request):
-    contexto = {};
-    email = request.POST.get("email", None)
-    password = request.POST.get("password", None)
-    return render(request, "login.html", contexto)
-"""
-
-
-
-
-
-
+    return render(request, "inicio.html", contexto);
 
 
 
